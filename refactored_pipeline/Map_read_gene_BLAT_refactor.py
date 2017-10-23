c5a86c8fcc9ae71a02b6dcaa3320d8f7dc7f1888
Billy Taj <billyc59@gmail.com>
2017-10-23 15:20:35 -0400

    various changes
    - changed redundant args
    - added a loop-killer to stop excess dna db traversal
    - added time probes at various interesting points

--- refactored_pipeline/Map_read_gene_BLAT_refactor.py
+++ refactored_pipeline/Map_read_gene_BLAT_refactor.py
@@ -8,8 +8,8 @@ import subprocess
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 import re
-
-
+import time
+from datetime import datetime as dt
 
 def sortbyscore(line):
     return line[11]
@@ -17,72 +17,74 @@ def sortbyscore(line):
     
 def gene_map(tsv, unmapped, read_seqs, gene2read_map, contig2read_map, mapped_reads):
     #don't bother with stupid conventions right now.  Just make sure all the input vars line up
-        print("are you ok?")
-        with open(tsv, "r") as tabfile:
-            query = ""
-            identity_cutoff = 85
-            length_cutoff = 0.65
-            score_cutoff = 60
-            Hits = []
-            b_contig_hit = False
-            
-            print("we're ok up until here")
-            for line in tabfile:
-                if len(line) < 2:
-                    continue
-                else:
-                    Hits.append(line.split("\t"))
-            Sorted_Hits = sorted(Hits, key = sortbyscore)
-            for line in Sorted_Hits:
-                if query in contig2read_map:
-                    b_contig_hit = True
-                else:
-                    b_contig_hit = False
-                if query == line[0]:
-                    continue
-                else:
-                    query = line[0]
-                    db_match = line[1]
-                    seq_identity = line[2]
-                    align_len = line[3]
-                    score = line[11]
-                    print("sorted hit out:", line)
-                    sys.exit()
-                if float(seq_identity) > float(identity_cutoff):
-                    if float(align_len) > len(read_seqs[query].seq) * length_cutoff:
-                        if float(score) > float(score_cutoff):
-                            if db_match in gene2read_map:
-                                if b_contig_hit:
-                                    for read in contig2read_map[query]:
-                                        if read not in gene2read_map[db_match]:
-                                            if read not in mapped_reads:
-                                                gene2read_map[db_match].append(read)
-                                                mapped_reads.add(read)
-                                else:
-                                    if query not in gene2read_map[db_match]:
-                                        gene2read_map[db_match].append(query)
-                                        mapped_reads.add(query)
-                            else:
-                                if b_contig_hit:
-                                    read_count = 0
-                                    for read in contig2read_map[query]:
+    start_internal_gene_map_time = time.clock()
+    print("are you ok?")
+    with open(tsv, "r") as tabfile:
+        query = ""
+        identity_cutoff = 85
+        length_cutoff = 0.65
+        score_cutoff = 60
+        Hits = []
+        b_contig_hit = False
+        
+        print("we're ok up until here")
+        for line in tabfile:
+            if len(line) < 2:
+                continue
+            else:
+                Hits.append(line.split("\t"))
+        Sorted_Hits = sorted(Hits, key = sortbyscore)
+        for line in Sorted_Hits:
+            if query in contig2read_map:
+                b_contig_hit = True
+            else:
+                b_contig_hit = False
+            if query == line[0]:
+                continue
+            else:
+                query = line[0]
+                db_match = line[1]
+                seq_identity = line[2]
+                align_len = line[3]
+                score = line[11]
+                #print("sorted hit out:", line)
+                #sys.exit()
+            if float(seq_identity) > float(identity_cutoff):
+                if float(align_len) > len(read_seqs[query].seq) * length_cutoff:
+                    if float(score) > float(score_cutoff):
+                        if db_match in gene2read_map:
+                            if b_contig_hit:
+                                for read in contig2read_map[query]:
+                                    if read not in gene2read_map[db_match]:
                                         if read not in mapped_reads:
+                                            gene2read_map[db_match].append(read)
                                             mapped_reads.add(read)
-                                            read_count += 1
-                                            if read_count == 1:
-                                                gene2read_map[db_match] = [read]
-                                            elif read_count > 1:
-                                                gene2read_map[db_match].append(read)
-                                else:
-                                    gene2read_map[db_match] = [query]
+                            else:
+                                if query not in gene2read_map[db_match]:
+                                    gene2read_map[db_match].append(query)
                                     mapped_reads.add(query)
-                            continue
-                unmapped.add(query) 
-
+                        else:
+                            if b_contig_hit:
+                                read_count = 0
+                                for read in contig2read_map[query]:
+                                    if read not in mapped_reads:
+                                        mapped_reads.add(read)
+                                        read_count += 1
+                                        if read_count == 1:
+                                            gene2read_map[db_match] = [read]
+                                        elif read_count > 1:
+                                            gene2read_map[db_match].append(read)
+                            else:
+                                gene2read_map[db_match] = [query]
+                                mapped_reads.add(query)
+                        continue
+            unmapped.add(query) 
+    end_internal_gene_map_time = time.clock()
+    print("internal gene map time:", end_internal_gene_map_time - start_internal_gene_map_time, "s")
                 
 def main():     
     
-    if(len(sys.argv) == 2):
+    if(len(sys.argv) == 3):
         if(sys.argv[1] == "arg_list"):
             arg_list  = []
             gene2read_map = {}
@@ -93,14 +95,14 @@ def main():
             genes = []
             contig2read_map = {}
 
-            with open (sys.argv[1], 'r') as arg_file:
+            with open (sys.argv[2], 'r') as arg_file:
                 arg_list = arg_file.readlines()
             arg_file.closed
             
             dna_db_path = arg_list[0].split('\n')[0]
             contig_to_read_path = arg_list[1].split('\n')[0]
             gene_file_to_read = arg_list[2]         .split('\n')[0]
-            final_gene_map = arg_list[3].split('\n')[0]
+            final_gene_map = arg_list[3].split('\n')[0] # just gets dumped-to.  doesn't matter what's in it, really
             contigs_not_BWA_path = arg_list[4].split('\n')[0]
             blat_results = arg_list[5].split('\n')[0]
             contig_not_aligned_path = arg_list[6].split('\n')[0] #this prog actually creates the not_BWA_not_BLAT
@@ -109,21 +111,25 @@ def main():
             #read_seqs = SeqIO.index(contigs_not_BWA_path, os.path.splitext(contigs_not_BWA_path)[1][1:])
             read_seqs = SeqIO.index(contigs_not_BWA_path, 'fasta')
             
+            start_read_contig_time = time.clock()
             with open(contig_to_read_path, "r") as contig_mapping:
                 for line in contig_mapping:
                     if len(line) > 5:
                         entry = line.strip("\n").split("\t")
                         contig2read_map[entry[0]] = entry[2:]
 
-            
+            end_read_contig_time = time.clock()
+            start_read_gene_map = time.clock()
             with open(gene_file_to_read, "r") as gene_mapping:
                 for line in gene_mapping:
                     if len(line) > 5:
                         entry = line.split("\t")
                         gene2read_map[entry[0]] = entry[3:]
-            
-            print("hhhuuuuuuuhhhhh")
+            end_read_gene_map = time.clock()
+            #print("hhhuuuuuuuhhhhh")
+            start_gene_map_time = time.clock()
             gene_map(blat_results, unmapped_reads, read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            end_gene_map_time = time.clock()
             
             # for each seq in the not_BWA section...
             for read in read_seqs:
@@ -136,11 +142,14 @@ def main():
 
             for read in unmapped_reads:
                 unmapped_seqs.append(read_seqs[read])
-            with open(output_file, "w") as outfile:
+                
+            with open(contig_not_aligned_path, "w") as outfile:
                 SeqIO.write(unmapped_seqs, outfile, "fasta")
                 
+            start_write_time = time.clock()    
+            #this is the problem area
             with open(gene_file_to_read, "w") as out_map:
-                for record in SeqIO.parse(DNA_DB, "fasta"):
+                for record in SeqIO.parse(dna_db_path, "fasta"):
                     if record.id in gene2read_map:
                         genes.append(record)
                         out_map.write(record.id + "\t" + str(len(record.seq)) + "\t" + str(len(gene2read_map[record.id])))
@@ -149,14 +158,21 @@ def main():
                             reads_count += 1
                         else:
                             out_map.write("\n")
-            with open(final_gene_map, "w") as outfile:
+            end_write_time = time.clock() 
+            final_name = final_gene_map.split(".")[0] + "_" + dna_db_path.split(".")[0] + ".fna"
+            with open(final_name, "w+") as outfile:
                 SeqIO.write(genes, outfile, "fasta")    
             
             print("PROGRAM DONE")
+            print("output file:", final_name)
+            print("read contig time:", end_read_contig_time - start_read_contig_time, "s")
+            print("final gene map time", end_gene_map_time - start_gene_map_time, "seconds")
+            print("write time:", end_write_time - start_write_time, "s")
+            
         else:
             print("not accepted")
     
-    elif(len(sys.argv) > 2):
+    elif(len(sys.argv) > 3):
         #old way of doing stuff
         DNA_DB = sys.argv[1]
         contig2read_file = sys.argv[2]
@@ -208,6 +224,7 @@ def main():
                 SeqIO.write(unmapped_seqs, outfile, "fasta")
 
         reads_count = 0
+        gene_map_key_count = len(gene2read_map)
         genes = []
         with open(gene2read_file, "w") as out_map:
             for record in SeqIO.parse(DNA_DB, "fasta"):
@@ -219,6 +236,9 @@ def main():
                         reads_count += 1
                     else:
                         out_map.write("\n")
+                if(reads_count >= gene_map_key_count):
+                    print("found all the keys.  stopping the loop")
+                    break
         with open(gene_file, "w") as outfile:
             SeqIO.write(genes, outfile, "fasta")
 
@@ -230,5 +250,8 @@ def main():
 
 
 if __name__ == "__main__":
+    print("job started at:", dt.today()) 
+    start_main_time = time.clock()
     main()
-    
+    end_main_time = time.clock()
+    print("total program time:", end_main_time - start_main_time, "seconds")
