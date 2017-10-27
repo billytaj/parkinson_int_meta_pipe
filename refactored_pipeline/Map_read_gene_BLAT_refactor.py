86f118f8cc7e99331bcce51c353c2e90cc9a3e43
Billy Taj <billyc59@gmail.com>
2017-10-27 14:53:45 -0400

    added back support for other files
    Added back support for all 4 sequence subsections:
    -> unpaired
    -> pair 1
    -> pair 2

--- refactored_pipeline/Map_read_gene_BLAT_refactor.py
+++ refactored_pipeline/Map_read_gene_BLAT_refactor.py
@@ -15,21 +15,22 @@ def sortbyscore(line):
     return line[11]
     
     
-def gene_map(tsv, unmapped, read_seqs, gene2read_map, contig2read_map, mapped_reads):
+def gene_map(blat_results, unmapped, read_seqs, gene2read_map, contig2read_map, mapped_reads):
     start_internal_gene_map_time = time.clock()
     
     align_len = 0
     seq_identity = 0
     score = 0
-    with open(tsv, "r") as tabfile:
+    db_match = ""
+
+    with open(blat_results, "r") as tabfile:
         query = ""
         identity_cutoff = 85.0
         length_cutoff = 0.65
         score_cutoff = 60.0
         Hits = []
         b_contig_hit = False
-        
-        
+
         for line in tabfile:
             if len(line) < 2:
                 continue
@@ -91,8 +92,17 @@ def main():
             arg_list  = []
             gene2read_map = {}
             mapped_reads = set()
-            unmapped_reads = set()
-            unmapped_seqs = []
+            unmapped_contig_reads = set()
+            unmapped_contig_seqs = []
+            
+            unmapped_unpaired_reads = set()
+            unmapped_unpaired_seqs = []
+            
+            unmapped_pair_1_reads = set()
+            unmapped_pair_1_seqs = []
+            
+            unmapped_pair_2_reads = set()
+            unmapped_pair_2_seqs = []
             reads_count = 0
             genes = []
             contig2read_map = {}
@@ -105,13 +115,32 @@ def main():
             contig_to_read_path = arg_list[1].split('\n')[0]
             gene_file_to_read = arg_list[2]         .split('\n')[0]
             final_gene_map = arg_list[3].split('\n')[0] # just gets dumped-to.  doesn't matter what's in it, really
+            #----------------------------
+            #repeats here in batches of 2
             contigs_not_BWA_path = arg_list[4].split('\n')[0]
-            blat_results = arg_list[5].split('\n')[0]
-            contig_not_aligned_path = arg_list[6].split('\n')[0] #this prog actually creates the not_BWA_not_BLAT
+            contig_blat_results = arg_list[5].split('\n')[0]
+            
+            unpaired_unmapped_not_BWA_path = arg_list[6].split('\n')[0]
+            unpaired_unmapped_not_BWA_blat_results = arg_list[7].split('\n')[0]
+            
+            pair_1_not_BWA_path = arg_list[8].split('\n')[0]
+            pair_1_not_BWA_blat_results = arg_list[9].split('\n')[0]
+            
+            pair_2_not_BWA_path = arg_list[10].split('\n')[0]
+            pair_2_not_BWA_blat_results = arg_list[11].split('\n')[0]
+            
+            
+            #------------------------------------------------
+            # open the files and put it through the gene map
+            
             print("contigs not BWA loc:", contigs_not_BWA_path)
             print("format:", os.path.splitext(contigs_not_BWA_path)[1][1:])
             #read_seqs = SeqIO.index(contigs_not_BWA_path, os.path.splitext(contigs_not_BWA_path)[1][1:])
-            read_seqs = SeqIO.index(contigs_not_BWA_path, 'fasta')
+            contig_read_seqs = SeqIO.index(contigs_not_BWA_path, 'fasta')
+            
+            unpaired_read_seqs = SeqIO.index(unpaired_unmapped_not_BWA_path, 'fasta')
+            pair_1_read_seqs = SeqIO.index(pair_1_not_BWA_path, 'fasta')
+            pair_2_read_seqs = SeqIO.index(pair_2_not_BWA_path, 'fasta')
             
             start_read_contig_time = time.clock()
             with open(contig_to_read_path, "r") as contig_mapping:
@@ -130,39 +159,83 @@ def main():
             end_read_gene_map = time.clock()
           
             start_gene_map_time = time.clock()
-            gene_map(blat_results, unmapped_reads, read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            gene_map(contig_blat_results, unmapped_contig_reads, contig_read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            gene_map(unpaired_unmapped_not_BWA_blat_results, unmapped_unpaired_reads, unpaired_read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            gene_map(pair_1_not_BWA_blat_results, unmapped_pair_1_reads, pair_1_read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            gene_map(pair_2_not_BWA_blat_results, unmapped_pair_2_reads, pair_2_read_seqs, gene2read_map, contig2read_map, mapped_reads)
+            
             end_gene_map_time = time.clock()
             
             # for each seq in the not_BWA section...
             start_first_write_time = time.clock()
-            for read in read_seqs:
-                unmapped_reads.add(read)
+            for read in contig_read_seqs:
+                if(read not in unmapped_contig_reads):
+                    #unmapped_reads is empty at start.  Only appends on the first hit of a new value
+                    unmapped_contig_seqs.append(contig_read_seqs[read])
+                unmapped_contig_reads.add(read)
+            
+            for read in unpaired_read_seqs:
+                if(read not in unmapped_unpaired_reads):
+                    #unmapped_reads is empty at start.  Only appends on the first hit of a new value
+                    unmapped_unpaired_seqs.append(unpaired_read_seqs[read])
+                unmapped_unpaired_reads.add(read)
+            
+            for read in pair_1_read_seqs:
+                if(read not in unmapped_pair_1_reads):
+                    #unmapped_reads is empty at start.  Only appends on the first hit of a new value
+                    unmapped_pair_1_seqs.append(pair_1_read_seqs[read])
+                unmapped_pair_1_reads.add(read)
+            
+            for read in pair_2_read_seqs:
+                if(read not in unmapped_pair_2_reads):
+                    #unmapped_reads is empty at start.  Only appends on the first hit of a new value
+                    unmapped_pair_2_seqs.append(pair_2_read_seqs[read])
+                unmapped_pair_2_reads.add(read)
                 
             
-            for read in unmapped_reads:
-                unmapped_seqs.append(read_seqs[read])
+            with open("contigs_not_aligned.fasta", "w+") as outfile:
+                SeqIO.write(unmapped_contig_seqs, outfile, "fasta")
+            
+            with open("unpaired_not_aligned.fasta", "w+") as outfile:
+                SeqIO.write(unmapped_unpaired_seqs, outfile, "fasta")
                 
-            with open(contig_not_aligned_path, "w") as outfile:
-                SeqIO.write(unmapped_seqs, outfile, "fasta")
+            with open("pair_1_not_aligned.fasta", "w+") as outfile:
+                SeqIO.write(unmapped_pair_1_seqs, outfile, "fasta")
+
+            with open("pair_2_not_aligned.fasta", "w+") as outfile:
+                SeqIO.write(unmapped_pair_2_seqs, outfile, "fasta")
+            
             end_first_write_time = time.clock()    
                 
-            start_write_time = time.clock()    
-            #this is the problem area
+            #--------------------------------------------
+            
+
+
+            #-----------------------------------------------------------------
+            #this stuff doesn't get repeated
+            start_write_time = time.clock()  
             with open(gene_file_to_read, "w") as out_map:
-                for record in SeqIO.parse(dna_db_path, "fasta"):
+                diagnostic_len_flag = True
+                dna_file = SeqIO.parse(dna_db_path, "fasta")
+                dna_record_count = 0
+                gene_map_record_count = 0
+                for record in dna_file:
+                    dna_record_count += 1
+                    #print("record id:", record.id)
                     if record.id in gene2read_map:
                         genes.append(record)
                         out_map.write(record.id + "\t" + str(len(record.seq)) + "\t" + str(len(gene2read_map[record.id])))
                         for read in gene2read_map[record.id]:
                             out_map.write("\t" + read.strip("\n"))
-                            reads_count += 1
-                        else:
-                            out_map.write("\n")
+                            gene_map_record_count += 1
+                    else:
+                        out_map.write("\n")
             end_write_time = time.clock() 
             final_name = final_gene_map.split(".")[0] + "_" + dna_db_path.split(".")[0] + ".fna"
             with open(final_name, "w+") as outfile:
                 SeqIO.write(genes, outfile, "fasta")    
-            
+            print("total number of dna in dna_db:", dna_record_count)
+            print("total number of gene map records to scan through:", gene_map_record_count)
             print("PROGRAM DONE")
             print("output file:", final_name)
             print("read contig time:", end_read_contig_time - start_read_contig_time, "s")
