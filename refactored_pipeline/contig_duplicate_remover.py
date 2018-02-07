46f01a449528bf6b5d43a94647da9e9d0d8812c9
Billy Taj <billyc59@gmail.com>
2018-02-07 16:34:47 -0500

    contig assembly integration complete
    This works

--- refactored_pipeline/contig_duplicate_remover.py
+++ refactored_pipeline/contig_duplicate_remover.py
@@ -7,11 +7,6 @@ import sys
 import os
 import pandas as pd
 
-
-
-def filter_contig_comsumption():
-
-
 if __name__ == "__main__":
     pair_1_path = sys.argv[1]       #fastq
     pair_2_path = sys.argv[2]       #fastq
@@ -20,22 +15,32 @@ if __name__ == "__main__":
     orphans_sam_path = sys.argv[5]   #sam
     output_path = sys.argv[6]       #just a location
     
+    print("pair 1:", pair_1_path)
+    print("pair 2:", pair_2_path)
+    print("orphans:", orphans_path)
+    print("pair sam:", pair_sam_path)
+    print("orphans sam:", orphans_sam_path)
+    print("output loc:", output_path)
+    
     pair_1_df = pd.read_csv(pair_1_path, header=None, names=[None], sep = '\n', skip_blank_lines = False)
+    pair_1_df = pd.DataFrame(pair_1_df.values.reshape(int(len(pair_1_df)/4), 4))
     pair_2_df = pd.read_csv(pair_2_path, header=None, names=[None], sep = '\n', skip_blank_lines = False)
+    pair_2_df = pd.DataFrame(pair_2_df.values.reshape(int(len(pair_2_df)/4), 4))
     orphans_df = pd.read_csv(orphans_path, header=None, names=[None], sep = '\n', skip_blank_lines = False)
+    orphans_df = pd.DataFrame(orphans_df.values.reshape(int(len(orphans_df)/4), 4))
     
     pair_1_df.columns = ["ID", "sequence", "junk", "quality"]
     pair_2_df.columns = ["ID", "sequence", "junk", "quality"]
     orphans_df.columns = ["ID", "sequence", "junk", "quality"]
     
-    pair_sam_df = pd.read_csv(pair_sam_path, header=None, sep="\t")
+    pair_sam_df = pd.read_csv(pair_sam_path, header=None, sep="\t", error_bad_lines=False)
     pair_sam_df.iloc[:, 1] = pair_sam_df.iloc[:, 1].apply(lambda x: bin(int(x))[2:].zfill(11)[8])
     
-    orphans_sam_df = pd.read_csv(orphans_sam_path, header=None, sep="\t")
+    orphans_sam_df = pd.read_csv(orphans_sam_path, header=None, sep="\t", error_bad_lines = False)
     orphans_sam_df.iloc[:, 1] = orphans_sam_df.iloc[:, 1].apply(lambda x: bin(int(x))[2:].zfill(11)[8])
 
     #select mapped and unmapped slices from pair
-    mapped_pair_sam_df = "@" + pair_sam_df.loc[pair_sam_df.iloc[:, 1] == "1"].iloc[:, 0]
+    mapped_pair_sam_df = "@" + pair_sam_df.loc[pair_sam_df.iloc[:, 1] == "1"].iloc[:, 0] 
     mapped_pair_sam_df = mapped_pair_sam_df.drop_duplicates()
     mapped_pair_sam_df.columns = ["ID"]
     
@@ -46,11 +51,11 @@ if __name__ == "__main__":
     unmapped_pair_sam_df = unmapped_pair_sam_df[~unmapped_pair_sam_df.isin(mapped_pair_sam_df)]
     
     #select mapped and unmapped slices from orphans
-    mapped_orphans_sam_df = orphans_sam_df.loc[orphans_sam_df.iloc[:, 1] == "1"].iloc[:, 0]
+    mapped_orphans_sam_df = "@" + orphans_sam_df.loc[orphans_sam_df.iloc[:, 1] == "1"].iloc[:, 0]
     mapped_orphans_sam_df = mapped_orphans_sam_df.drop_duplicates()
     mapped_orphans_sam_df.columns = ["ID"]
     
-    unmapped_orphans_sam_df = orphans_sam_df.loc[orphans_sam_df.iloc[:, 1] == "0"].iloc[:, 0]
+    unmapped_orphans_sam_df = "@" + orphans_sam_df.loc[orphans_sam_df.iloc[:, 1] == "0"].iloc[:, 0]
     unmapped_orphans_sam_df = unmapped_orphans_sam_df.drop_duplicates()
     unmapped_orphans_sam_df.columns = ["ID"]
     
@@ -60,4 +65,4 @@ if __name__ == "__main__":
     #write it
     pair_1_df[pair_1_df.ID.isin(unmapped_pair_sam_df)].to_csv(output_path+"pair_1.fastq", sep='\n', mode = "w+", header=False, index=False)
     pair_2_df[pair_2_df.ID.isin(unmapped_pair_sam_df)].to_csv(output_path+"pair_2.fastq", sep='\n', mode = "w+", header=False, index=False)
-    orphans_df[orphans_df.ID.isin(unmapped_orphans_sam_df)].to_csv(output_path+"orphans.fastq", sep='\n', mode = "w+". header=False, index=False)
+    orphans_df[orphans_df.ID.isin(unmapped_orphans_sam_df)].to_csv(output_path+"orphans.fastq", sep='\n', mode = "w+", header=False, index=False)
