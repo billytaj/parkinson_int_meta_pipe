dbd1ecc5c7a7bccb2a29196ba40d1fcda6e02fa9
Billy Taj <billyc59@gmail.com>
2018-03-27 21:39:06 -0400

    docker start
    Started crafting docker image setup
    cleaning up pathways (docker needs to install everything)

--- refactored_pipeline/mt_pipe_paths.py
+++ refactored_pipeline/mt_pipe_paths.py
@@ -1,23 +1,47 @@
 import os
-script_path = "/home/j/jparkins/billyc59/parkinson_int_meta_pipe/"
-refactor_path = script_path + "refactored_pipeline/"
 
-sort_reads = refactor_path + "sort_reads_refactor.py"
+#----------------------------------------------------------
+# external tools
 cdhit_dup = "/home/j/jparkins/mobolaji/Tools/CDHIT/cd-hit-v4.6.6-2016-0711/cd-hit-auxtools/cd-hit-dup"
 Timmomatic = "/home/j/jparkins/mobolaji/Tools/Trimmomatic/Trimmomatic-0.36/trimmomatic-0.36.jar"
 AdapterRemoval = "/home/j/jparkins/mobolaji/Tools/AdapterRemoval/adapterremoval-2.2.0/build/AdapterRemoval"
 vsearch = "/home/j/jparkins/mobolaji/Tools/vsearch/vsearch-2.4.2-linux-x86_64/bin/vsearch"
 UniVec_Core = "/home/j/jparkins/mobolaji/Databases/UniVec_Core.fasta"
 Flash = "/home/j/jparkins/mobolaji/Tools/Flash/FLASH-1.2.11/flash"
-Perl = "/home/j/jparkins/mobolaji/perl"
-Perl_Script_Dir = "/home/j/jparkins/mobolaji/Metatranscriptome_Scripts/Xuejian"
-Python = "python3" #"/home/j/jparkins/mobolaji/python"
 BWA = "/home/j/jparkins/mobolaji/Tools/BWA/bwa-0.7.5a/bwa"
 SAMTOOLS = "/home/j/jparkins/mobolaji/Tools/SAMTOOLS/samtools-1.3.1/samtools"
 BLAT = "/home/j/jparkins/mobolaji/Tools/pBLAT/pblat/pblat"
 DIAMOND = "/home/j/jparkins/mobolaji/Tools/Diamond/diamond"
 Blastdbcmd = "/home/j/jparkins/mobolaji/Tools/BLAST+/ncbi-blast-2.5.0+/bin/blastdbcmd"
 Makeblastdb = "/home/j/jparkins/mobolaji/Tools/BLAST+/ncbi-blast-2.5.0+/bin/makeblastdb"
+Adapter = "/home/j/jparkins/mobolaji/Tools/Trimmomatic/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa"
+Infernal = "/home/j/jparkins/mobolaji/Tools/Infernal/infernal-1.1.2-linux-intel-gcc/binaries/cmscan"
+Kaiju = "/home/j/jparkins/mobolaji/Tools/Kaiju/kaiju-v1.4.5-linux-x86_64-static/bin/kaiju"
+Kaiju2krona = "/home/j/jparkins/mobolaji/Tools/Kaiju/kaiju-v1.4.5-linux-x86_64-static/bin/kaiju2krona"
+ktImportText = "/home/j/jparkins/mobolaji/Tools/Krona/KronaTools-2.7/bin/ktImportText"
+Centrifuge = "/home/j/jparkins/mobolaji/Tools/Centrifuge/centrifuge/centrifuge"
+Centrifuge_report = "/home/j/jparkins/mobolaji/Tools/Centrifuge/centrifuge/centrifuge-kreport"
+kSLAM = "/home/j/jparkins/mobolaji/Tools/k-SLAM/k-SLAM/SLAM"
+Barrnap = "/home/j/jparkins/mobolaji/Tools/Barrnap/bin/barrnap"
+Priam = "/home/j/jparkins/mobolaji/Tools/PRIAM/PRIAM_search.jar"
+BLAST_dir = "/home/j/jparkins/mobolaji/Tools/BLAST/blast-2.2.26/bin/"
+WEVOTE = "/home/j/jparkins/mobolaji/Tools/WEVOTE/WEVOTE/run_WEVOTE_PIPELINE.sh"
+WEVOTEDB = "/home/j/jparkins/mobolaji/Tools/WEVOTE/WEVOTE/WEVOTEDB"
+
+#--------------------------------------------
+# reference paths
+script_path = "/home/j/jparkins/billyc59/parkinson_int_meta_pipe/"
+refactor_path = script_path + "refactored_pipeline/"
+
+#--------------------------------------------
+# custom scripts
+
+
+
+sort_reads = refactor_path + "sort_reads_refactor.py"
+Perl = "/home/j/jparkins/mobolaji/perl"
+Perl_Script_Dir = "/home/j/jparkins/mobolaji/Metatranscriptome_Scripts/Xuejian"
+Python = "python3" #"/home/j/jparkins/mobolaji/python"
 DNA_DB = "/scratch/j/jparkins/mobolaji/Microbial_cds_db/microbial_all_cds.fasta"
 DNA_DB_Prefix = os.path.splitext(DNA_DB)[0]
 DNA_DB_Extension = os.path.splitext(DNA_DB)[1]
@@ -25,8 +49,6 @@ Prot_DB = "/scratch/j/jparkins/mobolaji/NCBI_nr_db/nr"
 Host = "/home/j/jparkins/mobolaji/Databases/Mouse_cds.fasta"
 BBMap_Dir = "/home/j/jparkins/mobolaji/Tools/BBMap/bbmap"
 Fastqc = "/home/j/jparkins/mobolaji/Tools/FastQC/fastqc"
-Adapter = "/home/j/jparkins/mobolaji/Tools/Trimmomatic/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa"
-Infernal = "/home/j/jparkins/mobolaji/Tools/Infernal/infernal-1.1.2-linux-intel-gcc/binaries/cmscan"
 Rfam = "/home/j/jparkins/mobolaji/Databases/Rfam_rRNA.cm"
 Filter_rRNA = "/home/j/jparkins/mobolaji/Metatranscriptome_Scripts/Mobolaji/rRNA_Filter.py"
 duplicate_repopulate = refactor_path +  "duplicate_repopulation.py"
@@ -46,8 +68,7 @@ EC_Annotation_Prep = script_path + "EC_Prediction_Scripts/0_Preprocess_Input.py"
 Detect_Submit = script_path + "EC_Prediction_Scripts/1-1a_Detect_Submission.py"
 EC_Annotation_Post = script_path + "EC_Prediction_Scripts/4a_EC_Consolidation.py"
 Detect = "/home/j/jparkins/mobolaji/Tools/UpdatedDETECT_V2.0/detect_leon.py"
-Priam = "/home/j/jparkins/mobolaji/Tools/PRIAM/PRIAM_search.jar"
-BLAST_dir = "/home/j/jparkins/mobolaji/Tools/BLAST/blast-2.2.26/bin/"
+
 SWISS_PROT = "/home/j/jparkins/mobolaji/Databases/uniprot_sprot_annotated.fasta"
 
 Nodes = "/home/j/jparkins/mobolaji/Databases/taxdump/nodes.dmp"
@@ -55,20 +76,14 @@ Names = "/home/j/jparkins/mobolaji/Databases/taxdump/names.dmp"
 Annotated_taxid = script_path + "Read_Classification/Get_TaxID.py"
 Contrain_classification = script_path + "Read_Classification/Constrain_Classification.py"
 Classification_combine = script_path + "Read_Classification/Combine_WEVOTE.py"
-WEVOTE = "/home/j/jparkins/mobolaji/Tools/WEVOTE/WEVOTE/run_WEVOTE_PIPELINE.sh"
-WEVOTEDB = "/home/j/jparkins/mobolaji/Tools/WEVOTE/WEVOTE/WEVOTEDB"
+
 accession2taxid = "/scratch/j/jparkins/mobolaji/accession2taxid/accession2taxid"
-Kaiju = "/home/j/jparkins/mobolaji/Tools/Kaiju/kaiju-v1.4.5-linux-x86_64-static/bin/kaiju"
-Kaiju2krona = "/home/j/jparkins/mobolaji/Tools/Kaiju/kaiju-v1.4.5-linux-x86_64-static/bin/kaiju2krona"
-ktImportText = "/home/j/jparkins/mobolaji/Tools/Krona/KronaTools-2.7/bin/ktImportText"
-Centrifuge = "/home/j/jparkins/mobolaji/Tools/Centrifuge/centrifuge/centrifuge"
-Centrifuge_report = "/home/j/jparkins/mobolaji/Tools/Centrifuge/centrifuge/centrifuge-kreport"
-kSLAM = "/home/j/jparkins/mobolaji/Tools/k-SLAM/k-SLAM/SLAM"
+
 
 RPKM = script_path + "RPKM.py"
 
 #from rRNA_Filter
-Barrnap = "/home/j/jparkins/mobolaji/Tools/Barrnap/bin/barrnap"
+
 #Infernal = "/home/j/jparkins/mobolaji/Tools/Infernal/infernal-1.1.2-linux-intel-gcc/binaries/cmsearch"
 #Rfam = "/home/j/jparkins/mobolaji/Databases/Rfam_rRNA.cm"
 
