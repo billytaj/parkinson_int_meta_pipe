6259bda7d5b2c52d438ec9003c620b213bb80e47
Billy Taj <billyc59@gmail.com>
2018-06-27 13:14:44 -0400

    removed bad layer of parallel on diamond job
    Diamond jobs aren't actually split.  just a single run of each section. (contig, orphans, pair 1, pair 2)

--- refactored_pipeline/mt_pipe_refactor.py
+++ refactored_pipeline/mt_pipe_refactor.py
@@ -506,20 +506,21 @@ def main(input_folder, output_folder, system_op):
             if(not sync_obj.check_where_resume(output_folder + gene_annotation_DIAMOND_label)):
                 names = ["contigs", "orphans", "pair_1", "pair_2"]
                 #for i in range(1, 6):
-                for count in range(1,6):
-                    for item in names:
-                        inner_name = item + "_run_diamond_" + str(count)
-                        process = mp.Process(
-                            target = comm.create_pbs_and_launch,
-                            args = (
-                            gene_annotation_DIAMOND_label,
-                            comm.create_DIAMOND_annotate_command(gene_annotation_DIAMOND_label, gene_annotation_BLAT_label, item, count),
-                            True,
-                            inner_name
-                            )
+                #for count in range(1,6):
+                count = 0
+                for item in names:
+                    inner_name = item + "_run_diamond_" + str(count)
+                    process = mp.Process(
+                        target = comm.create_pbs_and_launch,
+                        args = (
+                        gene_annotation_DIAMOND_label,
+                        comm.create_DIAMOND_annotate_command(gene_annotation_DIAMOND_label, gene_annotation_BLAT_label, item, count),
+                        True,
+                        inner_name
                         )
-                        process.start()
-                        mp_store.append(process)
+                    )
+                    process.start()
+                    mp_store.append(process)
                 for item in mp_store:
                     item.join()
                 mp_store[:] = []
