d9911af04720715562605689a5cf08c597ba7fc8
Billy Taj <billyc59@gmail.com>
2018-07-13 11:30:45 -0400

    missing cleanup hack
    DETECT: litterbug

--- refactored_pipeline/mt_pipe_refactor.py
+++ refactored_pipeline/mt_pipe_refactor.py
@@ -658,6 +658,7 @@ def main(input_folder, output_folder, system_op, user_mode):
                 for item in mp_store:
                     item.join()  # wait for things to finish
                 mp_store[:] = []  # clear the list
+            sp.call(["rm", output_folder + "blast_hits*"]) #hack to do DETECT cleanup -> July 13, 2018
             EC_DETECT_end = time.time()
             
             
