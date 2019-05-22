95dc787e828aa04c9c194f01a40da7c3b2a57766
Billy Taj <billyc59@gmail.com>
2019-05-22 01:06:16 -0400

    completed data for humann2 taxa

--- prototypes/post-run-analysis/501_CecQ_n/clean_taxa_table.py
+++ prototypes/post-run-analysis/501_CecQ_n/clean_taxa_table.py
@@ -31,6 +31,8 @@ if __name__ == "__main__":
     
     taxa_split_df["Length"] = taxa_df["Length"]
     taxa_split_df["Reads"] = taxa_df["Reads"]
+    taxa_split_df["RPK"] = 0
+    taxa_split_df["RPK"] = taxa_split_df["RPK"].mask(taxa_split_df["RPK"] == 0, taxa_split_df["Reads"] / (taxa_split_df["Length"] / 1000))
     #species_df = taxa_split_df.groupby(["species"], as_index = False).sum()
     #print("Species read sum:", species_df["Reads"].sum())
     #print(species_df)
@@ -40,31 +42,31 @@ if __name__ == "__main__":
     #print("genus read sum:", genus_df["Reads"].sum())
     
     species_df = taxa_split_df[(taxa_split_df["species"] != "0")].groupby(["species"], as_index = False).sum()
-    print("Species read sum:", species_df["Reads"].sum())
+    print("Species read sum:", species_df["Reads"].sum(), "Species RPK sum:", species_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["species"] == "0")]
     
     genus_df = taxa_split_df[(taxa_split_df["genus"] != "0")].groupby(["genus"], as_index = False).sum()
-    print("genus read sum:", genus_df["Reads"].sum())
+    print("genus read sum:", genus_df["Reads"].sum(), "genus RPK sum:", genus_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["genus"] == "0")]
     
     family_df = taxa_split_df[taxa_split_df["family"] != "0"].groupby(["family"], as_index = False).sum()
-    print("family read sum:", family_df["Reads"].sum())
+    print("family read sum:", family_df["Reads"].sum(), "family RPK sum:", family_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["family"] == "0")]
     
     order_df = taxa_split_df[taxa_split_df["order"] != "0"].groupby(["order"], as_index = False).sum()
-    print("order read sum:", order_df["Reads"].sum())
+    print("order read sum:", order_df["Reads"].sum(), "order RPK sum:", order_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["order"] == "0")]
     
     class_df = taxa_split_df[taxa_split_df["class"] != "0"].groupby(["class"], as_index = False).sum()
-    print("class read sum:", class_df["Reads"].sum())
+    print("class read sum:", class_df["Reads"].sum(), "class RPK sum:", class_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["class"] == "0")]
     
     phylum_df = taxa_split_df[taxa_split_df["phylum"] != "0"].groupby(["phylum"], as_index = False).sum()
-    print("phylum read sum:", phylum_df["Reads"].sum())
+    print("phylum read sum:", phylum_df["Reads"].sum(), "phylum RPK sum:", phylum_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["phylum"] == "0")]
     
     kingdom_df = taxa_split_df[taxa_split_df["kingdom"] != "0"].groupby(["kingdom"], as_index = False).sum()
-    print("kingdom read sum:", kingdom_df["Reads"].sum())
+    print("kingdom read sum:", kingdom_df["Reads"].sum(), "kingdom RPK sum:", kingdom_df["RPK"].sum())
     taxa_split_df = taxa_split_df[(taxa_split_df["kingdom"] == "0")]
     
     unclassified_df = taxa_split_df.groupby(["root"], as_index = False).sum()
