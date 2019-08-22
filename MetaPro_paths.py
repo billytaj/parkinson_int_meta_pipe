a248b029ff1377a0bb31eb67d80f9e209c679ea8
Billy Taj <billyc59@gmail.com>
2019-08-22 12:20:53 -0400

    path update

--- MetaPro_paths.py
+++ MetaPro_paths.py
@@ -133,7 +133,7 @@ class tool_path_obj:
         self.contig_duplicate_remover   = os.path.join(script_path, "assembly_deduplicate.py")
         self.Map_reads_gene_BWA         = os.path.join(script_path, "ga_BWA_v2.py")
         self.Map_reads_gene_BLAT        = os.path.join(script_path, "ga_BLAT_v2.py")
-        self.Map_reads_prot_DMND        = os.path.join(script_path, "ga_Diamond_v2.py")
+        self.Map_reads_prot_DMND        = os.path.join(script_path, "ga_Diamond_v3.py")
         self.EC_Annotation_Post         = os.path.join(script_path, "ea_combine.py")
         self.Annotated_taxid            = os.path.join(script_path, "ta_taxid.py")
         self.Constrain_classification   = os.path.join(script_path, "ta_constrain_taxonomy_v2.py")
