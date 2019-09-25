89e06226957a2519a3440a1bf96194b3b7258ed1
Billy Taj <billyc59@gmail.com>
2019-09-25 19:46:04 -0400

    spacing issue

--- MetaPro_commands.py
+++ MetaPro_commands.py
@@ -1912,7 +1912,7 @@ class mt_pipe_commands:
         postprocess_command += os.path.join(diamond_ea_folder, "proteins.blastout") + " "
         postprocess_command += self.tool_path_obj.SWISS_PROT + " "
         postprocess_command += self.tool_path_obj.SWISS_PROT_map + " "
-        postprocess_command += os.path.join(diamond_folder, "gene_map.tsv")
+        postprocess_command += os.path.join(diamond_folder, "gene_map.tsv") + " "
         postprocess_command += os.path.join(final_folder, "proteins.ECs.All")
 
         COMMANDS_EC_Postprocess = [
