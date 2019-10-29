70cdef8192e7bfc3e6225f1f3668ec570fcc0ab7
Billy Taj <billyc59@gmail.com>
2019-10-29 18:04:00 -0400

    added in host reporting
    untested

--- Scripts/output_read_counts.py
+++ Scripts/output_read_counts.py
@@ -1,12 +1,6 @@
+#this code just makes the final stats reports
+#not much logic here
 import sys
-
-raw_sequence = sys.argv[1]
-quality_sequence = sys.argv[2]
-rRNA_sequence = sys.argv[3]
-mRNA_sequence = sys.argv[4]
-gene_to_read_map = sys.argv[5]
-ec_map = sys.argv[6]
-output_file = sys.argv[7]
 
 def fastq_count(fastq):
     lines = 0
@@ -37,53 +31,75 @@ def ec_count(map):
 
     return len(ecs)
 
-headings = []
-data = []
 
-headings.append("Total reads")
-raw_sequence_count = fastq_count(raw_sequence)
-data.append(str(int(raw_sequence_count)))
 
-headings.append("High quality reads")
-quality_sequence_count = fastq_count(quality_sequence)
-data.append(str(int(quality_sequence_count)))
+if __name__ == "__main__":
 
-headings.append("% high quality")
-quality_sequence_pct = quality_sequence_count / raw_sequence_count
-data.append("%.2f" % (quality_sequence_pct*100))
+    raw_sequence        = sys.argv[1]   #in: the raw, unfiltered input
+    quality_sequence    = sys.argv[2]   #in: the post-qual 
+    rRNA_sequence       = sys.argv[3]   #in: total rRNA
+    mRNA_sequence       = sys.argv[4]   #in: total mRNA
+    gene_to_read_map    = sys.argv[5]   #in: gene map: get number of reads
+    ec_map              = sys.argv[6]   #in: the gene-ec map: get number of unique ECs
+    combined_host       = sys.argv[7]   #in: combined host reads (dup'd)
+    output_file         = sys.argv[8]   #out: final chart
 
-headings.append("rRNA reads")
-rRNA_sequence_count = fastq_count(rRNA_sequence)
-data.append(str(int(rRNA_sequence_count)))
 
-headings.append("% rRNA reads")
-rRNA_sequence_pct = rRNA_sequence_count / raw_sequence_count
-data.append("%.2f" % (rRNA_sequence_pct*100))
+    headings = []
+    data = []
 
-headings.append("Putative mRNA reads")
-mRNA_sequence_count = fastq_count(mRNA_sequence)
-data.append(str(int(mRNA_sequence_count)))
+    headings.append("Total reads")
+    raw_sequence_count = fastq_count(raw_sequence)
+    data.append(str(int(raw_sequence_count)))
 
-headings.append("% putative mRNA reads")
-mRNA_sequence_pct = mRNA_sequence_count / raw_sequence_count
-data.append("%.2f" % (mRNA_sequence_pct*100))
+    headings.append("High quality reads")
+    quality_sequence_count = fastq_count(quality_sequence)
+    data.append(str(int(quality_sequence_count)))
 
-headings.append("Annotated mRNA reads")
-annotated_mRNA_count, genes_count = annotated_count(gene_to_read_map)
-data.append(str(int(annotated_mRNA_count)))
+    headings.append("% high quality")
+    quality_sequence_pct = quality_sequence_count / raw_sequence_count
+    data.append("%.2f" % (quality_sequence_pct*100))
+    
+    headings.append("host reads found in sample")
+    host_read_counts = fastq_count(combined_host)
+    data.append(str(int(host_read_counts)))
+    
+    headings.append("% host reads in sample")
+    host_pct = host_read_counts / raw_sequence_count
+    data.append("%.2f" % (host_pct * 100))
 
-headings.append("% of putative mRNA reads annotated")
-annotated_mRNA_pct = annotated_mRNA_count / mRNA_sequence_count
-data.append("%.2f" % (annotated_mRNA_pct*100))
+    headings.append("rRNA reads")
+    rRNA_sequence_count = fastq_count(rRNA_sequence)
+    data.append(str(int(rRNA_sequence_count)))
 
-headings.append("Unique transcripts")
-data.append(str(int(genes_count)))
+    headings.append("% rRNA reads")
+    rRNA_sequence_pct = rRNA_sequence_count / raw_sequence_count
+    data.append("%.2f" % (rRNA_sequence_pct*100))
 
-headings.append("Unique enzymes")
-unique_ec_count = ec_count(ec_map)
-data.append(str(int(unique_ec_count)))
+    headings.append("Putative mRNA reads")
+    mRNA_sequence_count = fastq_count(mRNA_sequence)
+    data.append(str(int(mRNA_sequence_count)))
 
-with open(output_file, "w") as outfile:
-    outfile.write("\t".join(headings))
-    outfile.write("\n")
-    outfile.write("\t".join(data))
+    headings.append("% putative mRNA reads")
+    mRNA_sequence_pct = mRNA_sequence_count / raw_sequence_count
+    data.append("%.2f" % (mRNA_sequence_pct*100))
+
+    headings.append("Annotated mRNA reads")
+    annotated_mRNA_count, genes_count = annotated_count(gene_to_read_map)
+    data.append(str(int(annotated_mRNA_count)))
+
+    headings.append("% of putative mRNA reads annotated")
+    annotated_mRNA_pct = annotated_mRNA_count / mRNA_sequence_count
+    data.append("%.2f" % (annotated_mRNA_pct*100))
+
+    headings.append("Unique transcripts")
+    data.append(str(int(genes_count)))
+
+    headings.append("Unique enzymes")
+    unique_ec_count = ec_count(ec_map)
+    data.append(str(int(unique_ec_count)))
+
+    with open(output_file, "w") as outfile:
+        outfile.write("\t".join(headings))
+        outfile.write("\n")
+        outfile.write("\t".join(data))
