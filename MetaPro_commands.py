10553188024a0e73eef255420e76498ca9a98970
Billy Taj <billyc59@gmail.com>
2020-10-28 17:53:22 -0400

    command change

--- MetaPro_commands.py
+++ MetaPro_commands.py
@@ -1435,32 +1435,34 @@ class mt_pipe_commands:
         subfolder       = os.path.join(self.Output_Path, stage_name)
         data_folder     = os.path.join(subfolder, "data")
         split_folder    = os.path.join(data_folder, "0_read_split", category)
+        merge_folder    = os.path.join(data_folder, "0_paired_merge")
         dep_loc         = os.path.join(self.Output_Path, dependency_stage_name, "final_results")
         jobs_folder     = os.path.join(data_folder, "jobs")
         self.make_folder(subfolder)
         self.make_folder(data_folder)
         self.make_folder(split_folder)
         self.make_folder(jobs_folder)
+        self.make_folder(merge_folder)
         
         merge_paired = ">&2 echo merging paired reads | "
         merge_paired += self.tool_path_obj.Python + " "
         merge_paired += self.tool_path_obj.GA_merge_paired + " "
         merge_paired += os.path.join(dep_loc, "pair_1.fastq") + " "
         merge_paired += os.path.join(dep_loc, "pair_2.fastq") + " "
-        merge_paired += os.path.join(split_folder, "paired_merged.fastq")
+        merge_paired += os.path.join(merge_folder, "paired_merged.fastq")
         
         split_fastq = ">&2 echo splitting fastq for " + category + " GA | "
         split_fastq += "split -l " + str(int(self.tool_path_obj.GA_chunksize) * 4) + " "        
-        split_fastq += os.path.join(split_folder, "paired_merged.fastq") + " "
+        split_fastq += os.path.join(merge_folder, "paired_merged.fastq") + " "
         split_fastq += "--additional-suffix .fastq" + " "
         split_fastq += "-d" + " "
-        split_fastq += os.path.join(split_folder,  + category + "_")
+        split_fastq += os.path.join(split_folder, category + "_")
         
         make_marker = "touch" + " "
         make_marker += os.path.join(jobs_folder, marker_file)
         
         COMMANDS_GA_prep_fastq = [merge_paired + " && " + split_fastq + " && " + make_marker]
-        
+        return COMMANDS_GA_prep_fastq
     
     def create_split_ga_fastq_data_command(self, stage_name, dependency_stage_name, category, marker_file):
         subfolder       = os.path.join(self.Output_Path, stage_name)
